283e3c5ee673ca763e57b004a2d63f005307e5ee
Andugula <Sudhakar.Andugula@wwecorp.com>
2020-05-21 13:51:40 +0530

    monetization summary fix and network consumption code

---
+++ models/fds_igm/base/fact_ig_consumption_story.sql
@@ -0,0 +1,8 @@
+{{
+  config(
+	materialized='ephemeral'
+    
+  )
+}}
+
+select * from fds_igm.fact_ig_consumption_story
--- models/fds_yt/aggregate_intermediate/wwe_engagement_measures.sql
+++ models/fds_yt/aggregate_intermediate/wwe_engagement_measures.sql
@@ -36,8 +36,10 @@ from
 (select report_date_dt,report_date,channel_name,country_code,duration,time_uploaded,video_id,views,watch_time_minutes,likes,dislikes,subscribers_gained,
 subscribers_lost,dim_source_type_id,title from {{ref('rpt_yt_wwe_engagement_daily')}} where report_date_dt 
 between current_date - 52 and current_date - 1 and dim_source_type_id in (2,3)) x
-left join {{ ref('yt_amg_content_groups') }} y     
-on x.video_id=y.yt_id
+left join (select distinct yt_id,channel_name,amg_content_group from {{ ref('yt_amg_content_groups') }}) y 
+/* 5/15/2020/ Hima / added distinct on amg content groups to eliminate duplicates */   
+on x.video_id=y.yt_id and x.channel_name=y.channel_name  
+/* 5/15/2020 / Hima / added join on channel_name along with video_id due to duplicates with NULL channel name in yt_amg_content_group table for some video_id's */
 left join
 (select distinct upper(iso_alpha2_ctry_cd) as iso_alpha2_ctry_cd ,country_nm,region_nm from {{ ref('dim_region_country') }}
 where etl_source_name='Youtube') d
