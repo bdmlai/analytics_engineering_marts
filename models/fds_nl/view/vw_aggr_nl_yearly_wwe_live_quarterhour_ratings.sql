d30e87ef5b4f03b7d52cbd880913f0cecf727d66
Andugula <Sudhakar.Andugula@wwecorp.com>
2020-05-22 14:48:56 +0530

    comment

---
+++ models/fds_nl/view/vw_aggr_nl_yearly_wwe_live_quarterhour_ratings.sql
@@ -0,0 +1,33 @@
+{{
+  config({
+	"materialized": 'view',
+	"post-hook": ["COMMENT ON COLUMN fds_nl.vw_aggr_nl_yearly_wwe_live_quarterhour_ratings.broadcast_month IS 'Show broadcast month'"]
+	})
+}}
+
+
+--QH ratings for WWE program(yearly)
+
+/*
+*************************************************************************************************************************************************
+   Date        : 06/12/2020
+   Version     : 1.0
+   TableName   : vw_aggr_nl_yearly_wwe_live_quarterhour_ratings
+   Schema	   : fds_nl
+   Contributor : Sudhakar Andugula
+   Description : WWE Live Quarter Hour Rating Yearly Aggregate View consist of rating details of all WWE Live - RAW, SD, NXT Programs to be rolled up from Quarter Hour Viewership Ratings Table on yearly-basis
+*************************************************************************************************************************************************
+*/
+
+
+select b.cal_year as broadcast_year, src_broadcast_network_id, src_playback_period_cd, src_demographic_group, src_program_id, interval_starttime, interval_endtime,
+--Duration Weighted Averages are taking for avg_audience_proj_000, avg_audience_pct and avg_pct_nw_cvg_area here..
+(sum(avg_audience_proj_000 * interval_duration)/sum(nvl2(avg_audience_proj_000, interval_duration, null))) as avg_audience_proj_000,
+(sum(avg_audience_pct * interval_duration)/sum(nvl2(avg_audience_pct, interval_duration, null))) as avg_audience_pct,
+(sum(avg_pct_nw_cvg_area * interval_duration)/sum(nvl2(avg_pct_nw_cvg_area, interval_duration, null))) as avg_pct_nw_cvg_area, 
+sum(avg_viewing_hours_units) as avg_viewing_hours_units
+from (select broadcast_date_id, src_broadcast_network_id, src_playback_period_cd, src_demographic_group, src_program_id, interval_starttime, interval_endtime, interval_duration, avg_viewing_hours_units, avg_audience_proj_000, avg_audience_pct, avg_pct_nw_cvg_area
+from {{ref('fact_nl_quaterhour_viewership_ratings')}}
+where (src_broadcast_network_id, src_program_id) in ((5, 296881), (5, 339681), (5, 436999), (81, 898521), (10433, 1000131))) a
+left join {{ref('dim_date')}} b on a.broadcast_date_id = b.dim_date_id
+group by 1,2,3,4,5,6,7 ;
