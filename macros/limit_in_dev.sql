41fd8bf7bc3d79a791a1f799f3eba0460269fe3d
Pawar, Satyendra <satyendra.pawar@wwecorp.com>
2020-10-20 11:53:12 -0400

    Revert "Merge branch 'feature/viewership' into 'master'"
    This reverts merge request !117

--- macros/limit_in_dev.sql
+++
@@ -1,9 +0,0 @@
-{% macro limit_in_dev(timestamp) %}
-    -- this filter will only apply during a dev run
-    {% if target.name == 'dev' %}
-    
-    where {{ timestamp }} >= dateadd('month', -1, current_date)
-    
-    {% endif %}
-    
-{% endmacro %}
