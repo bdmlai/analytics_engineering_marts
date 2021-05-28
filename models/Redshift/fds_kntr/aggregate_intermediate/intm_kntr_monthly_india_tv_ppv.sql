c7191d216ce317ee81f6591645b2dcf385e23ae0
sandugul <sudhakar.andugula@wwecorp.com>
2021-05-28 17:03:54 +0530

    Code for BIOPS-669

--- models/Redshift/fds_kntr/aggregate_intermediate/intm_kntr_monthly_india_tv_ppv.sql
+++ models/Redshift/fds_kntr/aggregate_intermediate/intm_kntr_monthly_india_tv_ppv.sql
@@ -5,28 +5,51 @@
   })
 }}
 
-SELECT  a.*,
+SELECT  a.src_country,
+        a.src_demographic_group,
+        a.broadcast_date,
+        a.series_name,
+        a.series_episode_name,
+        a.broadcast_network_prem_type,
+        a.live_flag,
+        a.hd_flag,
+        a.aud,
+        a.duration_mins,
+        a.week_start_date,
+        a.watched_mins,
         b.ppv_start_date,
         b.ppv_end_date,
         dateadd(day,6,a.week_start_date) AS week_end_date
 FROM
     (
         (   
-            SELECT * FROM {{source('fds_kntr','fact_kntr_wwe_telecast_data')}} 
+            SELECT  src_country,
+                    src_demographic_group,
+                    broadcast_date,
+                    series_name,
+                    series_episode_name,
+                    broadcast_network_prem_type,
+                    live_flag,
+                    hd_flag,
+                    aud,
+                    duration_mins,
+                    week_start_date,
+                    watched_mins
+            FROM    {{source('fds_kntr','fact_kntr_wwe_telecast_data')}} 
         ) a
         JOIN
         (   
-            SELECT  event_name,
-                    trunc(dateadd(day,1,event_date))    AS ppv_start_date, 
-                    trunc(dateadd(day,8,event_date))    AS ppv_end_date 
-            FROM    {{source('fds_nplus','rpt_network_ppv_liveplusvod')}}
-            WHERE   event_date >= '2019-01-01'  AND 
-                    data_level = 'Live'         AND 
-                    platform = 'Total'
+            SELECT  ppv_nm,
+                    trunc(dateadd(day,1,event_dttm))    AS ppv_start_date, 
+                    trunc(dateadd(day,8,event_dttm))    AS ppv_end_date 
+            FROM    {{source('cdm','dim_event')}}
+            WHERE   event_dttm >= '2019-01-01'  AND 
+                    ppv_nm <> ' '               AND
+                    cancelled_by IS NULL        
         ) b
         ON  a.broadcast_date >= b.ppv_start_date      AND
             a.broadcast_date <= b.ppv_end_date        AND
-            lower(a.series_episode_name) LIKE ('%'+lower(b.event_name)+'%')
+            lower(replace(series_episode_name, ' ', '')) = lower(replace(ppv_nm, ' ', '')) 
     )    
 WHERE   broadcast_date >= '2019-01-01'      AND
         src_country = 'india'               AND
