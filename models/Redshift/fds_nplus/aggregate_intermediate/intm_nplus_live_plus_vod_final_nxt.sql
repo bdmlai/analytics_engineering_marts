297dd25d66215935c1c98e11b3f702d906c1fb84
LAKSHMANAN MURUGESAN <lakshmanan.murugesan@wwecorp.com>
2021-06-21 07:12:39 +0530

    liveplusvod divide by zero fix

--- models/Redshift/fds_nplus/aggregate_intermediate/intm_nplus_live_plus_vod_final_nxt.sql
+++ models/Redshift/fds_nplus/aggregate_intermediate/intm_nplus_live_plus_vod_final_nxt.sql
@@ -1,16 +1,16 @@
 {{ config({ "schema": 'fds_nplus', "materialized": 'table',"tags": 'nxt_live_vod_kickoff' }) }}
 SELECT
         a.*                                                            ,
-        (a.views*1.00)/a.prev_month_views-1                             AS monthly_per_change_views,
-        (a.views*1.00)/a.prev_year_views-1                              AS yearly_per_change_views ,
+        (a.views*1.00)/nullif(a.prev_month_views,0)-1                             AS monthly_per_change_views,
+        (a.views*1.00)/nullif(a.prev_year_views,0)-1                              AS yearly_per_change_views ,
         (EXTRACT(EPOCH FROM ((end_time) - (start_time)))/60::numeric)+1 AS duration                ,
         row_number() OVER (PARTITION BY
                            a.platform ORDER BY
                            a.views DESC)                                                                                                                                                                                                                                                                     AS overall_rank                    ,
         CASE WHEN a.event_brand                        = 'NXT' AND     nxt_yearly_rank>0 THEN nxt_yearly_rank ELSE NULL END                                                                                                                                                                                                     AS yearly_rank  ,
         CASE WHEN lower(a.event)                    LIKE '%wrestlemania%' THEN 'Tier 1' WHEN lower(a.event) LIKE '%royal rumble%' AND     lower(a.event) NOT LIKE '%greatest%' THEN 'Tier 1' WHEN lower(a.event) LIKE '%summerslam%' THEN 'Tier 1' WHEN lower(a.event) LIKE '%survivor series%' THEN 'Tier 1' ELSE 'Tier 2' END AS tier         ,
-        CASE WHEN (a.views*1.00)/a.prev_month_views-1 >= 0 THEN '1' ELSE '0' END                                                                                                                                                                                                                                                AS monthly_color,
-        CASE WHEN (a.views*1.00)/a.prev_year_views-1  >= 0 THEN '1' ELSE '0' END                                                                                                                                                                                                                                                AS yearly_color ,
+        CASE WHEN (a.views*1.00)/nullif(a.prev_month_views,0)-1 >= 0 THEN '1' ELSE '0' END                                                                                                                                                                                                                                                AS monthly_color,
+        CASE WHEN (a.views*1.00)/nullif(a.prev_year_views,0)-1  >= 0 THEN '1' ELSE '0' END                                                                                                                                                                                                                                                AS yearly_color ,
         CASE WHEN a.event_date                         =
                         (
                                 SELECT
