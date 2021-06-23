f85e769ddebb931e3be75ae5f0377ce46d175cfe
Chandran, Rahul <rahul.chandran@wwecorp.com>
2021-06-23 13:32:48 -0400

    Revert "Merge branch 'BIOPS-723_UAT' into 'master'"
    This reverts merge request !340

--- models/Redshift/fds_nl/aggregate_intermediate/intm_nl_lite_log_est.sql
+++ models/Redshift/fds_nl/aggregate_intermediate/intm_nl_lite_log_est.sql
@@ -3,62 +3,16 @@
 		"materialized": 'ephemeral'
   })
 }}
-SELECT
-    a.showdbid,
-    title,
-    subtitle,
-    episodenumber,
-    airdate,
-    inpoint,
-    outpoint,
-    (DATEADD(hr, 12, (DATEADD(sec, b.est_time_diff, (airdate || ' ' || substring(trim(inpoint), 1,
-    8))::TIMESTAMP))))                                                          AS inpoint_24hr_est,
-    ((substring((DATEADD(sec, 30, inpoint_24hr_est)), 1, 17) || '00')::TIMESTAMP) AS
-    modified_inpoint,
-    ((substring((DATEADD(sec, (((substring(duration, 1, 2))::INT * 60 * 60) + ((substring(duration,
-    4, 2))::INT * 60) + ((substring(duration, 7, 2))::INT) + 30), inpoint_24hr_est)), 1, 17) ||
-    '00')::TIMESTAMP) AS modified_outpoint,
-    segmenttype,
-    comment,
-    matchtype,
-    talentactions,
-    move,
-    finishtype,
-    recorddate,
-    fileid,
-    duration,
-    additionaltalent,
-    announcers,
-    matchtitle,
-    venuelocation,
-    venuename,
-    issegmentmarker,
-    logentrydbid,
-    logentryguid,
-    loggername,
-    logname,
-    masterclipid,
-    modifieddatetime,
-    networkassetid,
-    sponsors,
-    weapon,
-    season,
-    source_ffed_name
-FROM
-    {{source('udl_nplus','raw_lite_log')}} a
-JOIN
-    {{ref('intm_nl_est_time_diff')}} b
-ON
-    a.showdbid = b.showdbid
-WHERE
-    airdate IS NOT NULL
-AND inpoint IS NOT NULL
-AND duration IS NOT NULL
-AND inpoint <> ' '
-AND duration <> ' '
-AND LOWER(trim(logname)) NOT IN
-    (
-        SELECT
-            logname
-        FROM
-            {{ref('intm_nl_excluded_logs')}})
+
+select a.showdbid, title, subtitle, episodenumber, airdate, inpoint, outpoint, 
+(dateadd(hr, 12, (dateadd(sec, b.est_time_diff, (airdate || ' ' || substring(trim(inpoint), 1, 8))::timestamp)))) as inpoint_24hr_est, 
+((substring((dateadd(sec, 30, inpoint_24hr_est)), 1, 17) || '00')::timestamp) as modified_inpoint,
+((substring((dateadd(sec, (((substring(duration, 1, 2))::int * 60 * 60) + ((substring(duration, 4, 2))::int * 60) 
++ ((substring(duration, 7, 2))::int) + 30), inpoint_24hr_est)), 1, 17) || '00')::timestamp) as modified_outpoint,
+segmenttype, comment, matchtype, talentactions, move, finishtype, recorddate, fileid, duration, additionaltalent, announcers, 
+matchtitle, venuelocation, venuename, issegmentmarker, logentrydbid, logentryguid, loggername, logname, 
+masterclipid, modifieddatetime, networkassetid, sponsors, weapon, season, source_ffed_name 
+FROM {{source('udl_nplus','raw_lite_log')}} a 
+join {{ref('intm_nl_est_time_diff')}}  b on a.showdbid = b.showdbid
+where airdate is not null and inpoint is not null and duration is not null
+and inpoint <> ' ' and duration <> ' '
